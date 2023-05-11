6b214eb3e60d1eaf00c01cb6d188782c6040a701
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-05-12 01:23:23 +0300

    install packages,add config variables in one file and require dotenv once

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cookieParser = require("cookie-parser");
-
+const errorHandler = require("./middlewars/ErrorMiddleware")
 const router = require("./routes")
 const app = express()
 
@@ -9,5 +9,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api/v1", router)
+app.use(errorHandler)
 
 module.exports = app;
--- backend/config/enviroment.js
+++ backend/config/enviroment.js
@@ -1,13 +1,21 @@
 const dotenv = require("dotenv")
 dotenv.config({ path: "backend/.env" })
 
-const { DEVELOPMENT_URI, PRODUCTION_URI, PORT, NODE_ENV } = process.env
+const { DEVELOPMENT_URI, PRODUCTION_URI, PORT, NODE_ENV, JSONWEBTOKEN_SECRET_KEY, COOKIE_EXPIRES_TIME } = process.env
 
 const config = {
     database: {
         uri: NODE_ENV === "development" ? DEVELOPMENT_URI : PRODUCTION_URI
     },
+    node_env: NODE_ENV,
     port: PORT || 5000
+    ,
+    JsonWebTokenConfig: {
+        secret_key: JSONWEBTOKEN_SECRET_KEY || ''
+    },
+    cookieConfig: {
+        expiresTime: COOKIE_EXPIRES_TIME
+    },
 }
 
 module.exports = config;
