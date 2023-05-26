2be0e5d615e501ed4ac2284cd8ddf87fefed684e
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2023-05-26 09:07:39 +0300

    uploading my work on course page to get my api

--- frontend/attendant-tracker/src/components/TableViewerCourse/TableViewerCourse.jsx
+++ frontend/attendant-tracker/src/components/TableViewerCourse/TableViewerCourse.jsx
@@ -3,5 +3,25 @@ import axios from 'axios';
 import { WrapperViewer, ColumnBar, ColumnTitle, ColumnRecord, UpdateBtn, DelBtn, AddAttendance, Input } from '../TableViewer/TableViewer.styles';
 import Popup from '../Popup/Popup';
 const TableViewerCourse=()=>{
-    
-}
+    const arr=["course_id","title","dept_name","book"];
+    const arr1=[{course_id:"ECOM3422",title:"DBMS",dept_name:"Engineering",book:"DBMS7thedition"}];
+    return(
+        <WrapperViewer>
+            <ColumnBar>
+                {arr.map((element,index)=>(<ColumnTitle key={index}>{element}</ColumnTitle>))}
+            </ColumnBar>
+            {arr1.map((el,index)=>(
+                <ColumnRecord>
+                    <ColumnTitle key={index}>{el.course_id}</ColumnTitle>
+                    <ColumnTitle key={index}>{el.title}</ColumnTitle>
+                    <ColumnTitle key={index}>{el.dept_name}</ColumnTitle>
+                    <ColumnTitle key={index}>{el.book}</ColumnTitle>
+                </ColumnRecord>
+                
+            ))}
+        </WrapperViewer>
+
+
+    );
+};
+export default TableViewerCourse;
--- frontend/attendant-tracker/src/pages/coursePage/CoursePage.jsx
+++ frontend/attendant-tracker/src/pages/coursePage/CoursePage.jsx
@@ -2,21 +2,19 @@ import React, { useState } from 'react'
 import NavBar from '../../components/navBar/NavBar';
 import {Wrapper,InternalWrapper,SearchBar,Input,Button} from '../StudentPage/StudentPage.styles';
 import { Label } from '../../components/navBar/navBar.styles';
-import TableViewer from '../../components/TableViewer/TableViewer';
+import TableViewerCourse from '../../components/TableViewerCourse/TableViewerCourse';
 const CoursePage = () => {
-
+  const[TextString,setTextString]=useState("");
   return (
     <Wrapper>
     <NavBar/>
     <InternalWrapper>
         <SearchBar>
-        <Label>Search For Student</Label>
-            <Input />
-            <Button >Search by name</Button>
-            <Button>Search by id</Button>
-            <Button >Search by phone</Button>
+        <Label>Search For Course</Label>
+            <Input onBlur={(e)=>{setTextString(e.target.value);
+            console.log(e.target.value);}}/>
         </SearchBar>
-        <TableViewer/>
+        <TableViewerCourse/>
     </InternalWrapper>
     </Wrapper>
   )
