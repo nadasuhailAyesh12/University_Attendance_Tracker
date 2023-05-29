915e3fbcecf1cecfc38b196290f38e287859b92c
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-05-29 21:19:57 +0300

    convert it from csv to excel

--- backend/Controllers/CSVFilesController.js
+++ backend/Controllers/CSVFilesController.js
@@ -3,6 +3,7 @@ const { join } = require('path')
 const csv = require('csv-parser');
 const db = require('../models/seeding/Connection');
 const studentRepository = require('../models/queries/StudentQuery');
+const exceljs = require('exceljs')
 
 const insertfromCsvFileToDatabase = async (req, res, next) => {
     try {
@@ -26,33 +27,66 @@ const insertfromCsvFileToDatabase = async (req, res, next) => {
     }
 }
 
-const exportStudentsWhoAttendLessthan25Percent = async (req, res, next) => {
-    try {
-        const { course_id, sec_id } = req.params;
-        const data = await studentRepository.getStudentsWhoAttendLessthan25Percent(course_id, sec_id)
+const exportStudentsWhoAttendLessthan25Percent = async (req, res) => {
+    const { course_id, sec_id } = req.params;
+    const workbook = new exceljs.Workbook();
+    const worksheet = workbook.addWorksheet('Sheet 1');
 
-        // Convert data to CSV format
-        const csvData = data.map(row => Object.values(row).join(','));
-        csvData.unshift("Student_id")
+    // Add column headers to the worksheet
+    studentRepository.getStudentsWhoAttendLessthan25Percent(course_id, sec_id).then((data) => {
+        console.log(data);
+        const headers = Object.keys(data[0]);
+        worksheet.addRow(headers);
+        // Add data rows to the worksheet
+        data.forEach(row => {
+            const values = Object.values(row);
+            worksheet.addRow(values);
+        });
 
-        // Define the file path and name
-        const filePath = join(__dirname, '..', "..", "./assets", `./ student_dropped${Date.now()}.csv`);
+        // Set the content type and headers for the download
+        res.setHeader(
+            'Content-Type',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        );
+        res.setHeader(
+            'Content-Disposition',
+            'attachment; filename=data.xlsx'
+        );
 
-        // Write CSV data to a file
-        fs.writeFileSync(filePath, csvData.join('\n'));
+        // Stream the workbook as an Excel file to the response
+        return workbook.xlsx.write(res);
 
-        // Set the response headers to trigger a download
-        res.setHeader('Content-Type', 'text/csv');
-        res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-
-        // Stream the file to the response
-        const fileStream = fs.createReadStream(filePath);
-        fileStream.pipe(res);
-    } catch (error) {
-        console.error('Error exporting data:', error);
-        res.status(500).send('An error occurred while exporting data');
-    }
+    }).then(() => {
+        res.end();
+    })
+        .catch(error => {
+            // Handle any errors
+            console.error(error);
+            res.status(500).send('Error exporting data to Excel');
+        })
 }
+// Convert data to CSV format
+//     const csvData = data.map(row => Object.values(row).join(','));
+//     csvData.unshift("Student_id")
+
+//     // Define the file path and name
+//     const filePath = join(__dirname, '..', "..", "./assets", `./ student_dropped${Date.now()}.csv`);
+
+//     // Write CSV data to a file
+//     fs.writeFileSync(filePath, csvData.join('\n'));
+
+//     // Set the response headers to trigger a download
+//     res.setHeader('Content-Type', 'text/csv');
+//     res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
+
+//     // Stream the file to the response
+//     const fileStream = fs.createReadStream(filePath);
+//     fileStream.pipe(res);
+// } catch (error) {
+//     console.error('Error exporting data:', error);
+//     res.status(500).send('An error occurred while exporting data');
+// }
+// }
 
 const csvFileController = { insertfromCsvFileToDatabase, exportStudentsWhoAttendLessthan25Percent }
 module.exports = csvFileController
