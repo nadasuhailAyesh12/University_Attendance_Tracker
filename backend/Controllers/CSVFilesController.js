edbafc2188600c1275ca3abc56016cb44636dfcd
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-05-29 22:14:30 +0300

    make insert functionality

--- backend/Controllers/CSVFilesController.js
+++ backend/Controllers/CSVFilesController.js
@@ -1,37 +1,32 @@
-const fs = require('fs')
-const { join } = require('path')
-const csv = require('csv-parser');
 const db = require('../models/seeding/Connection');
 const studentRepository = require('../models/queries/StudentQuery');
 const exceljs = require('exceljs')
+const XLSX = require('xlsx');
 
 const insertfromCsvFileToDatabase = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        fs.createReadStream(req.files.file.tempFilePath)
-            .pipe(csv())
-            .on('data', (data) => {
-                // Insert data into the database
-                db.none('INSERT INTO attendance VALUES($1,$2)', [id, data.id])
-                    .catch(error => {
-                        return next(error)
-                    });
-            })
-            .on('end', () => {
-                console.log('Data insertion completed.');
-            });
-    }
-    catch (err) {
-        return next(err)
+    const { sec_id, course_id, lecture_id } = req.params;
 
-    }
+    const workbook = XLSX.readFile(req.files.file.tempFilePath);
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+    jsonData.map(data => {
+        db.none('INSERT INTO attendance  VALUES ($1, $2, $3,$4)', [lecture_id, data.id, sec_id, course_id])
+            .then(() => {
+                console.log('Data inserted successfully.');
+                res.end()
+            })
+            .catch(error => {
+                console.error('Error inserting data:', error);
+                res.status(500).send('Error while inserting data to database');
+            });
+    })
 }
 
 const exportStudentsWhoAttendLessthan25Percent = async (req, res) => {
     const { course_id, sec_id } = req.params;
     const workbook = new exceljs.Workbook();
     const worksheet = workbook.addWorksheet('Sheet 1');
-
     // Add column headers to the worksheet
     studentRepository.getStudentsWhoAttendLessthan25Percent(course_id, sec_id).then((data) => {
         console.log(data);
@@ -65,28 +60,7 @@ const exportStudentsWhoAttendLessthan25Percent = async (req, res) => {
             res.status(500).send('Error exporting data to Excel');
         })
 }
-// Convert data to CSV format
-//     const csvData = data.map(row => Object.values(row).join(','));
-//     csvData.unshift("Student_id")
 
-//     // Define the file path and name
-//     const filePath = join(__dirname, '..', "..", "./assets", `./ student_dropped${Date.now()}.csv`);
-
-//     // Write CSV data to a file
-//     fs.writeFileSync(filePath, csvData.join('\n'));
-
-//     // Set the response headers to trigger a download
-//     res.setHeader('Content-Type', 'text/csv');
-//     res.setHeader('Content-Disposition', 'attachment; filename=data.csv');
-
-//     // Stream the file to the response
-//     const fileStream = fs.createReadStream(filePath);
-//     fileStream.pipe(res);
-// } catch (error) {
-//     console.error('Error exporting data:', error);
-//     res.status(500).send('An error occurred while exporting data');
-// }
-// }
 
 const csvFileController = { insertfromCsvFileToDatabase, exportStudentsWhoAttendLessthan25Percent }
 module.exports = csvFileController
--- backend/routes/FileRouter.js
+++ backend/routes/FileRouter.js
@@ -2,6 +2,6 @@ const csvFileController = require("../Controllers/CSVFilesController");
 const fileRouter = require('express').Router();
 
 fileRouter.get('/:course_id/:sec_id', csvFileController.exportStudentsWhoAttendLessthan25Percent)
-fileRouter.post('/:id', csvFileController.insertfromCsvFileToDatabase)
+fileRouter.post('/:course_id/:sec_id/:lecture_id', csvFileController.insertfromCsvFileToDatabase)
 
 module.exports = fileRouter
