4bd3aa6b00fc66c37392c2214c141caa4023f62d
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2023-05-29 18:44:09 +0300

    finishing-lectures

--- frontend/attendant-tracker/src/pages/lecturePage/LecturePage.jsx
+++ frontend/attendant-tracker/src/pages/lecturePage/LecturePage.jsx
@@ -14,8 +14,14 @@ const LecturePage = () => {
     const [DummyState,setDummyState]=useState(0);
     const [courses,setCourses]=useState([]);
     const [departments,setDepartments]=useState([]);
+    const [isSelectedAdd,setIsSelectedAdd]=useState(false);
+    const [isYearMistake,setIsYearMistake]=useState(false);
+    const [ObjToAdd,setObjToAdd]=useState({lecture_id:"",sec_id:"",room_number:"",building:"",day:"",start_time:"",end_time:"",semester:"",year:""})
     const closeFirstPopUp=()=>{
         setFirstPop(false);
+    }
+    const closeAddPopUp=()=>{
+        setIsSelectedAdd(false);
     }
     const getAllCourses=async()=>{
         try{
@@ -39,6 +45,22 @@ const LecturePage = () => {
         }
         
     }
+    const AddLectures=async()=>{
+        console.log("added");
+        try{
+            
+            if(isNaN(ObjToAdd.year)){
+                setIsYearMistake(true);
+            }
+            else{
+             const{lecture_id, year,sec_id,room_number,building,day,start_time,end_time,semester} =ObjToAdd;
+            const response=await axios.post("http://localhost:5000/api/v1/lecture",{course_id:course_id,lecture_id,year,sec_id,building,day,start_time,end_time,semester,room_number});
+            console.log(response);
+            }
+        }catch(error){
+            console.log(error);
+        }
+    }
     useEffect(()=>{
         getAllCourses();
         getAllDepartments();
@@ -53,6 +75,9 @@ const LecturePage = () => {
             <Button onClick={()=>{
                 setDummyState((prev)=>prev+1)
             }}>Search for lectures</Button>
+            <Button onClick={()=>{
+                setIsSelectedAdd(true);
+            }}>Add Lectures</Button>
             <Label>Select course_id</Label>
             <Selector>
                 {courses.map((el,index)=>
@@ -84,6 +109,86 @@ const LecturePage = () => {
                 setDummyState((prev)=>prev+1)
             }}>Load lectures</UpdateBtn>
         </Popup>
+        <Popup onClose={closeAddPopUp} isOpen={isSelectedAdd}>
+            <Label>Lecture ID</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.lecture_id=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>Section ID</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.sec_id=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>Room Number</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.room_number=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>Building</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.building=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>day</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.day=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>Start Time</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.start_time=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>End Time</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.end_time=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>Semester</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.semester=e.target.value;
+                    return p;
+                })
+            }}/>
+            <Label>Year</Label>
+            <Input onBlur={(e)=>{
+                setObjToAdd((prev)=>{
+                    const p=prev;
+                    p.year=e.target.value;
+                    return p;
+                })
+            }}/>
+            { isYearMistake && <Label style={{color:'red'}}>Year should be a number</Label> }
+            <UpdateBtn onClick={()=>{
+                console.log("fuck");
+                AddLectures();
+            }}>Add Lectures</UpdateBtn>
+        </Popup>
+
     </Wrapper>
   )
 }
