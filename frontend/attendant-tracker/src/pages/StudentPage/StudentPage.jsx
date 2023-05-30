e8bb9cf0b611f92afbd9bbe593bea8691d3d6560
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2023-05-30 04:14:12 +0300

    adding loading with 3 req params

--- frontend/attendant-tracker/src/pages/StudentPage/StudentPage.jsx
+++ frontend/attendant-tracker/src/pages/StudentPage/StudentPage.jsx
@@ -13,10 +13,12 @@ const StudentPage = ({role,setRole}) => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [isOpenToImport,setIsOpenToImport]=useState(false);
   const [lectureId,setLectureId] = useState(''); 
-  const [dept_name_field,setDept_name_field]=useState("");
-  const [course_id,setCourse_id]=useState("");
+  const [dept_name_field,setDept_name_field]=useState("Engineering");
+  const [course_id,setCourse_id]=useState("ECOM3422");
   const [courses,setCourses]=useState([]);
   const [departments,setDepartments]=useState([]);
+  const [sec_id,setSec_id]=useState("201");
+  const [isFOpen,setIsFOpen]=useState(true);
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
       // Perform some action when Enter key is pressed
@@ -30,6 +32,9 @@ const StudentPage = ({role,setRole}) => {
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
+  const closeFOpen=()=>{
+    setIsFOpen(false);
+  }
   const handleUpload = () => {
     if (selectedFile) {
       const formData = new FormData();
@@ -71,7 +76,7 @@ const getAllDepartments=async()=>{
 useEffect(()=>{
   getAllDepartments();
   getAllCourses();
-})
+},[])
   return (
     <Wrapper>
     <NavBar role={role}/>
@@ -82,24 +87,32 @@ useEffect(()=>{
             <Button onClick={()=>{
               setIsOpenToImport(true);
             }}>Import CSV File</Button>
-            <Selector>
+            <Selector onChange={(e)=>{
+              setCourse_id(e.target.value);
+            }}>
                 {courses.map((el,index)=>
                 (
                     <option key={index}>{el.course_id}</option>
                 ))}
             </Selector>
-            <Selector>
+            <Selector onChange={(e)=>{
+              setDept_name_field(e.target.value);
+            }}>
                 {departments.map((el,index)=>
                 (
                     <option key={index}>{el.dept_name}</option>
                 ))}
             </Selector>
+            <Label>Sec id</Label>
+            <Input onBlur={(e)=>{
+              setSec_id(e.target.value);
+            }}/>
         </SearchBar>
         <SearchBar>
             <Input onBlur={(e)=>setTextString(e.target.value)} onKeyDown={handleKeyPress}/>
             <Button onClick={()=>setSearchParams("name")}>Search</Button>
         </SearchBar>
-        <TableViewer WhichSection="student" SearchParams={SearchParams} TextString={TextString}/>
+        <TableViewer WhichSection="student" SearchParams={SearchParams} TextString={TextString} course_id={course_id} dept_name={dept_name_field} sec_id={sec_id}/>
     </InternalWrapper>
     <Popup isOpen={isOpenToImport} onClose={closePopup}>
       <Label>Lecture ID</Label>
@@ -109,6 +122,15 @@ useEffect(()=>{
       <UpdateBtn onClick={()=>{
         handleUpload();
       }}>send CSV FILE to DATABASE</UpdateBtn>
+    </Popup>
+    <Popup isOpen={isFOpen} onClose={closeFOpen}>
+      <Label>Sec ID</Label>
+      <Input/>
+      <UpdateBtn onClick={()=>{
+        console.log("i am clicked");
+        setSec_id(sec_id);
+        console.log(sec_id);
+      }}>Load</UpdateBtn>
     </Popup>
     </Wrapper>
   )
