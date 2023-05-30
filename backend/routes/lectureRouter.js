ce2a6ef1e0ad7ff86711beda7fe635806b5744be
nadasuhailAyesh12 <missnadaayesh@gmail.com>
2023-05-30 21:17:29 +0300

    make attendance  status

--- backend/routes/lectureRouter.js
+++ backend/routes/lectureRouter.js
@@ -2,6 +2,7 @@ const lectureController = require('../Controllers/LectureController');
 const lectureRouter = require('express').Router();
 
 lectureRouter.get('/search/:course_id/:dept_name/:options', lectureController.searchLecture)
+lectureRouter.get('/attendanceStatus/:course_id/:sec_id', lectureController.AttendanceStatus)
 lectureRouter.get('/:dept_name/:course_id', lectureController.getLectures)
 lectureRouter.post('/', lectureController.insertLecture)
 lectureRouter.put('/:id/:oldCourse_id/:oldSec_id', lectureController.updateLecture)
